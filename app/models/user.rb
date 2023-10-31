304f1e0c25e51625b44147b0593cb6e29ffecba3
Jordan Biego <jordanbiego@gmail.com>
2023-10-31 14:40:28 -0300

    feat: :art: Add redirect_to custom route when loging out

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name, :nickname, :birthday, :encrypted_password
+  validates_uniqueness_of :nickname, uniqueness: { scope: :name, message: 'Nickname already taken!' }
 end
