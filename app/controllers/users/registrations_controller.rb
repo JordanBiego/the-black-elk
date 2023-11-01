17ff82265cd35bad6d86875106db4f1d3b6d9512
Jordan Biego <jordanbiego@gmail.com>
2023-11-01 15:46:06 -0300

    feat: :art: Finish sign-in/sign-out layout first version

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -32,7 +32,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :birthday, :email])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
